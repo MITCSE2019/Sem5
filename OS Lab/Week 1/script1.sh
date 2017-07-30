532be2b93c347b25616b22a66f5022a1a684aff8
akashrajr1 <akashrajr1@gmail.com>
2017-07-30 10:29:17 +0530

    Script for OSL Week 1

---
+++ OS Lab/Week 1/script1.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#(i)
+mkdir ~/dir
+cd ~/dir
+mkdir sub1 sub2
+
+#(ii)
+cd sub1
+echo "content1" > axy
+echo "content2" > Abc
+echo "content2" > xyz
+
+#(iii)
+cp xyz ../sub2
+
+#(iv)
+echo -e "Users: `who| wc -l`\nFiles: `ls -l | grep "^-" | wc -l`" > number
+
+#(v)
+ls -p | grep -v \ | grep -E "^[aA]"
+
+#(v)
+echo "No of files: `ls -l | grep "^-" | wc -l`"
+
+#(vi)
+cat <<DELIMITER > hello.c
+#include <stdio.h>
+
+void main(){
+printf("%s","Hello");
+} 
+DELIMITER
+gcc hello.c -o hello && ./hello
