bc72322780dcba67417cf1737cafe284978e48d2
Akashraj <akashrajr1@gmail.com>
2017-08-02 10:52:02 -0700

    Restoring code

--- Algorithms Lab/Week 1/adjacency_list1.c
+++ Algorithms Lab/Week 1/adjacency_list1.c
@@ -1,59 +1,77 @@
-//Baap ka code!
 #include <stdio.h>
 #include <stdlib.h>
-struct adj
+
+typedef struct node
 {
-    int data;
-    struct adj* next;
-};
+	int val;
+	struct node * next;
+}NODE;
 int main()
 {
-    int V;
-    int i;
-    printf("Enter number of vertices ");
-    scanf("%d",&V);
-    struct adj* a[V];
-    for(i=0;i<V;i++){
-        a[i]=(struct adj*)malloc(sizeof(struct adj));
-        a[i]->data=i;
-        a[i]->next=NULL;
-    }
-    while(1){
-            printf("Enter vertex pair \n");
-            int x,y;
-            scanf("%d %d",&x,&y);
-        struct adj* node1;
-        struct adj* temp=(struct adj*)malloc(sizeof(struct adj));
-        node1=a[x];
-        while(node1->next!=NULL){
-            node1=node1->next;
-        }
-        temp->data=y;
-        node1->next=temp;
-        temp->next=NULL;
+	int v,e1,e2;
+	int i,j;
+	NODE ** graph;
+	scanf("%d",&v);
+	int adj_matrix[v][v];
+	graph = (NODE **) calloc(v+1,sizeof(NODE *));
+	for(i=0;i<v;i++)
+	{
+		graph[i]=(NODE *) malloc(sizeof(NODE));
+		graph[i]->val=i+1;
+		graph[i]->next=NULL;
+	}
+	int ch;
+	while(1)
+	{
+		scanf("%d",&e1);
+		scanf("%d",&e2);
+		adj_matrix[e1-1][e2-1]=1;
+		NODE * head = graph[e1-1];
+		while(head->next!=NULL)
+		{
+			head=head->next;
+		}
+		NODE *n = (NODE *) malloc(sizeof(NODE));
+		n->val=e2;
+		head->next=n;
+		n->next=NULL;
 
-        struct adj* temp2=(struct adj*)malloc(sizeof(struct adj));
-        node1=a[y];
-        while(node1->next!=NULL){
-            node1=node1->next;
-        }
-        temp2->data=x;
-        node1->next=temp2;
-        temp2->next=NULL;
-        printf("Continue? ");
-        int n;
-        scanf("%d",&n);
-        if(n!=1)
-            break;
-    }
-    for( i=0;i<V;i++){
-        struct adj* node;
-        node=a[i];
-        while(node!=NULL){
-            printf("%d ",node->data);
-            node=node->next;
-        }
-        printf("\n");
-    }
-    return 0;
+		head = graph[e2-1];
+		while(head->next!=NULL)
+			head=head->next;
+
+		NODE * n2 = (NODE *) malloc(sizeof(NODE));
+		n2->val = e1;
+		head->next=n2;
+		n2->next=NULL;
+
+		printf("Enter more?\n");
+		scanf("%d",&ch);
+		if(ch==0)
+			break;
+	}
+
+	for(i=0;i<v;i++)
+		for(j=0;j<v;j++)
+			if(adj_matrix[i][j]!=1)
+				adj_matrix[i][j]=0;
+
+	for(i=0;i<v;i++)
+	{
+		NODE * head = graph[i];
+		while(head!=NULL)
+			{
+				printf("%d->",head->val);
+				head=head->next;
+			}
+		printf("NULL\n");
+	}
+
+	for(i=0;i<v;i++)
+		{
+			for(j=0;j<v;j++)
+				printf("%d ",adj_matrix[i][j]);
+			printf("\n");
+		}
+
 }
