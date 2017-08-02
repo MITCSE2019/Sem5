f56f140702d2ab63d80ba127d0b320d3f2177eeb
Pramod Shenoy <PramodShenoy@users.noreply.github.com>
2017-08-02 21:27:07 +0530

    adding Amazon select's implementation

---
+++ Algorithms Lab/Week 1/adjacency_list.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+	int val;
+	struct node * next;
+}NODE;
+int main()
+{
+	int v,e1,e2;
+	int i,j;
+	NODE ** graph;
+	scanf("%d",&v);
+	graph = (NODE **) calloc(v+1,sizeof(NODE *));
+	for(i=0;i<v;i++)
+	{
+		graph[i]=(NODE *) malloc(sizeof(NODE));
+		graph[i]->val=i+1;
+		graph[i]->next=NULL;
+	}
+	int ch;
+	while(1)
+	{
+		scanf("%d",&e1);
+		scanf("%d",&e2);
+
+		NODE * head = graph[e1-1];
+		while(head->next!=NULL)
+		{
+			head=head->next;
+		}
+		NODE *n = (NODE *) malloc(sizeof(NODE));
+		n->val=e2;
+		head->next=n;
+		n->next=NULL;
+
+		head = graph[e2-1];
+		while(head->next!=NULL)
+			head=head->next;
+
+		NODE * n2 = (NODE *) malloc(sizeof(NODE));
+		n2->val = e1;
+		head->next=n2;
+		n2->next=NULL;
+
+		printf("Enter more?\n");
+		scanf("%d",&ch);
+		if(ch==0)
+			break;
+	}
+
+	for(i=0;i<v;i++)
+	{
+		NODE * head = graph[i];
+		while(head!=NULL)
+			{
+				printf("%d->",head->val);
+				head=head->next;
+			}
+		printf("NULL\n");
+	}
+
+}
