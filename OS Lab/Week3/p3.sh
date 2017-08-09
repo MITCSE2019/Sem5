9a391968acd592aaa2582344d5032fef5f979a91
Prajjwal Rai <prajjwalrai.11@gmail.com>
2017-08-09 16:52:37 +0530

    file checking

---
+++ OS Lab/Week3/p3.sh
@@ -0,0 +1,15 @@
+for files in $1*
+do
+    t=0
+    while read input
+    do
+        if [ "$input" == "$files" ]
+        then
+            t=1
+        fi
+    done <"TOC.txt"
+    if [ $t -eq 0 ]
+    then
+        echo $files>>"TOC.txt"
+    fi
+done
