5d89ba54be35049aaefee5049f6c4c685f943e98
Prajjwal Rai <prajjwalrai.11@gmail.com>
2017-08-19 00:21:55 +0530

    Depth first search

---
+++ Algorithms Lab/Week 4/p2.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int visited[100];
+int push[20];
+int pop[20];
+int push_i,pop_i;
+ 
+void dfs(int a[][100],int i,int v)
+{
+    visited[i]=1;
+    int j=0;
+    for(j=0;j<v;j++)
+    {
+        if(j==i)
+            continue;
+ 
+       
+        if(!visited[j] && a[i][j]!=0)
+        {
+            visited[j]=1;
+            push[push_i]=j;
+            push_i++;
+            dfs(a,j,v);
+            pop[pop_i]=j;
+            pop_i++;
+        }
+       
+    }
+}
+ 
+int main()
+{
+    int a[100][100];
+    int v,i,j;
+    push_i=0;
+    pop_i=0;
+ 
+    printf("Enter no of vertices : ");
+    scanf("%d",&v);
+ 
+    printf("Enter Adjacency Graph : ");
+    for (int i = 0; i < v; i++)
+    {
+        for (int j = 0; j < v; j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+ 
+    for (int i = 0; i < v; i++)
+    {
+        visited[i]=0;
+    }
+ 
+    for (int i = 0; i < v; i++)
+    {
+        if(!visited[i])
+        {
+            visited[i]=1;
+            push[push_i]=i;
+            push_i++;
+            dfs(a,i,v);
+            pop[pop_i]=i;
+            pop_i++;
+        }
+    }
+    printf("PUSH ORDER : ");
+    for (int i = 0; i < push_i; i++)
+    {
+       
+        printf("%d  ",push[i] + 1);
+    }
+    printf("\n");
+ 
+    printf("POP ORDER : ");
+    for (int i = 0; i < pop_i; i++)
+    {
+       
+        printf("%d  ",pop[i] + 1);
+    }
+    printf("\n");
+}
+
+//Contributed by Sarthak Bhol
