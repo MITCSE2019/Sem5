7b38d56dfabc9f5315846a3a43358c1b1de7f669
Prajjwal Rai <prajjwalrai.11@gmail.com>
2017-10-05 11:13:19 +0530

    Warshall's Algo

---
+++ Algorithms Lab/Week 10/p1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main ()
+{
+	int i, j, k, n, opc = 0;
+	int a[20][20], b[20][20];
+	printf( "\nEnter number of vertices" );
+	scanf( "%d",&n );
+	printf("\nEnter Adjacency Graph" );
+	for ( i = 0; i < n; i++ )
+	{
+		for ( j = 0; j < n; j++ )
+		{
+			scanf( "%d", &a[i][j] );
+			b[i][j] = a[i][j];
+		}
+	}
+	for ( k = 0; k < n; k++ )
+	{
+		for ( i = 0; i < n; i++ )
+		{
+			for ( j = 0; j < n; j++ )
+			{
+				if ( b[i][j] == 0 )
+				{
+					opc++;
+					b[i][j] =  b[i][j] || ( b[i][k] && b[k][j] );
+				}
+			}
+		}
+	}
+	printf("\n The resultant matrix is :\n" );
+	for ( i = 0; i < n; i++ )
+	{
+		for ( j = 0; j < n; j++ )
+		{
+			printf( "%d\t", b[i][j] );
+		}
+		printf( "\n" );
+	}
+	printf("\nOperation Count : %d", opc );
+}
