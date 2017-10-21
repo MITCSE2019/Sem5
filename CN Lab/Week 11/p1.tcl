acba64e9c7e66356aba04c267d58405eaa96cd1a
Prajjwal Rai <prajjwalrai.11@gmail.com>
2017-10-21 10:27:23 +0530

    tcl file for q1

---
+++ CN Lab/Week 11/p1.tcl
@@ -0,0 +1,75 @@
+set ns [new Simulator]
+
+$ns color 1 red
+$ns color 2 green
+
+set nt [open simulate.nam w]
+$ns namtrace-all $nt
+
+set tr [open simulate.tr w]
+$ns trace-all $tr
+
+proc finish {} {
+	global ns nt tr
+	$ns flush-trace
+	close $nt
+	close $tr
+	exec nam simulate.nam &
+	exit 0
+}
+
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+
+$ns duplex-link $n0 $n1 2Mb 10ms DropTail
+$ns duplex-link $n0 $n2 2Mb 10ms DropTail
+
+
+$ns queue-limit $n0 $n2 5
+
+
+$ns duplex-link-op $n0 $n1 orient right
+$ns duplex-link-op $n0 $n2 orient right-down
+
+
+$ns duplex-link-op $n0 $n1 queuePos 0.5
+
+set udp [new Agent/UDP]
+$ns attach-agent $n0 $udp
+
+set null [new Agent/Null]
+$ns attach-agent $n1 $null
+$ns connect $udp $null
+$udp set fid_ 1
+
+set tcp [new Agent/TCP]
+$tcp set class_ 1
+$ns attach-agent $n0 $tcp
+
+
+set sink [new Agent/TCPSink]
+$ns attach-agent $n2 $sink
+$ns connect $tcp $sink
+$tcp set fid_ 2
+
+set ftp [new Application/FTP]
+$ftp attach-agent $tcp
+$ftp set type_ FTP
+
+
+
+set cbr [new Application/Traffic/CBR]
+$cbr attach-agent $udp
+$cbr set type_ CBR
+$cbr set packet_size_ 1000
+$cbr set rate_ 1mb
+$cbr set random_ false
+
+$ns at 0.1 "$cbr start"
+$ns at 4.5 "$cbr stop"
+$ns at 1.0 "$ftp start"
+$ns at 2.0 "$ftp stop"
+$ns at 5.0 "finish"
+
+$ns run
