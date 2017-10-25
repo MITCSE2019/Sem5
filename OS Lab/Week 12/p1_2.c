cd6c1e06b6a1b7369409054a9bca1b9fa7090e29
Prajjwal Rai <prajjwalrai.11@gmail.com>
2017-10-25 20:05:41 +0530

    SSTF

---
+++ OS Lab/Week 12/p1_2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define start 53
+
+void main()
+{
+    int a[20], i, n, mov = 0, min = 1000, visited[20] = { 0 }, index, k = 0, cur = start;
+    printf( "Enter request size\n" );
+    scanf( "%d", &n );
+    printf( "Enter requests" );
+    for ( i = 0; i < n; i++ )
+    {
+        scanf( "%d", &a[i] );
+    }
+    while ( k < n )
+    {
+        for ( i = 0; i < n; i++ )
+        {
+            if ( abs( cur - a[i] ) < min && visited[i] == 0 )
+            {
+                min = abs( cur - a[i] );
+                index = i;
+            }
+        }
+        mov+= min;
+        visited[index] = 1;
+        k++;
+        min = 1000;
+        cur = a[index];
+    }
+    printf( "The total head movement is : %d\n", mov );
+}
