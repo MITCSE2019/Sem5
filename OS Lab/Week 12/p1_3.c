479f14875fa614d4d01dceb613bbef7c64e6c974
Prajjwal Rai <prajjwalrai.11@gmail.com>
2017-10-30 11:39:41 +0530

    SCAN

--- OS Lab/Week 12/p1_3.c
+++ OS Lab/Week 12/p1_3.c
@@ -8,12 +8,10 @@ void main()
     printf( "Enter request size\n" );
     scanf( "%d", &n );
     printf( "Enter requests" );
+    
     for ( i = 0; i < n; i++ )
     {
         scanf( "%d", &a[i] );
-    }
-    for ( i = 0; i < n; i++ )
-    {
         if ( a[i] < start )
         {
             b[k++] = a[i];
@@ -23,6 +21,7 @@ void main()
             c[l++] = a[i];
         }
     }
+    
     b[k++] = 0;
     for ( i = 0; i < k - 1; i++ )
     {
